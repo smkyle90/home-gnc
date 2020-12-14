1274dfd9137c98abad3c30bceb00441629c935b3
Scott Kyle <scott.m.kyle@gmail.com>
2020-12-14 11:32:20 -0500

    added alias names for some nodes

--- lib/objects/__init__.py
+++ lib/objects/__init__.py
@@ -1,3 +1,2 @@
-from .Controller import Controller
 from .Person import Person
 from .Router import Router
--- navigation.py
+++ navigation.py
@@ -3,83 +3,12 @@ import argparse
 import json
 import time
 
-import graph_tool.all as gt
 import numpy as np
 import yaml
 import zmq
 
-from lib.funcs import scan_networks
+from lib.funcs import calibrate, scan_networks
 from lib.objects import Person, Router
-
-
-def build_graph(g, nodes_info):
-    for node_data in nodes_info.values():
-        new_node = g.vertex(node_data["NODE_ID"], add_missing=True)
-        g.vp["name"][new_node] = node_data["NODE_ID"]
-        g.vp["pos"][new_node] = (node_data["COORD"][0], node_data["COORD"][1])
-
-    for _node, node_data in nodes_info.items():
-        src = node_data["NODE_ID"]
-        for tgt in node_data["CONNECTS_TO"]:
-
-            e = g.add_edge(src, tgt)
-
-            q_src = np.array(g.vp["pos"][src])
-            q_tgt = np.array(g.vp["pos"][tgt])
-            q_dst = q_src - q_tgt
-            q_dst = np.linalg.norm(q_dst)
-
-            g.ep["dist"][e] = q_dst
-
-    return g
-
-
-def reconstruct_path(src, tgt, preds):
-    """Reconstruct a path back form the target
-    to the source using the predecessor array that
-    is returned from the dijkstra or astar searches.
-
-    Args:
-
-    Returns:
-
-    """
-
-    vertices = []
-    while int(tgt) != int(src):
-        vertices.append(tgt)
-        tgt = preds.a[tgt]
-
-    vertices.reverse()
-
-    return vertices
-
-
-def get_route(g, src, task_list):
-    src_node = g.vertex(src)
-    dists = g.ep["dist"]
-
-    task_order = [src]
-    node_order = [src]
-
-    if src in task_list:
-        task_list.remove(src)
-
-    while task_list:
-        dist_map, pred_map = gt.astar_search(g, source=src_node, weight=dists)
-        min_dist = np.inf
-        next_node = src_node
-        for t in task_list:
-            if dist_map[t] < min_dist:
-                next_node = t
-
-        node_order.extend(reconstruct_path(src_node, next_node, pred_map))
-
-        src_node = g.vertex(next_node)
-        task_list.remove(next_node)
-        task_order.append(next_node)
-
-    return task_order, node_order
 
 
 def main(config):
@@ -105,20 +34,17 @@ def main(config):
 
     scott = Person(est_state[0, 0], est_state[1, 0], est_covar)
 
-    n_wait = 0
     while True:
         scott.predict(process_covar)
         print("Logger: Predict Step Complete.")
 
-        n_meas = 0
-        if n_wait % 5 == 0:
-            print("Logger: Scanning for network data.")
-            net_data = scan_networks(interface, sudo)
+        print("Logger: Scanning for network data.")
+        net_data = scan_networks(interface, sudo)
 
-            print("Logger: Network Data received.")
+        print("Logger: Network Data received.")
 
-            n_meas = scott.update(net_data, router_loc)
-            print("Logger: Update Step Complete using {} measurements.".format(n_meas))
+        n_meas = scott.update(net_data, router_loc)
+        print("Logger: Update Step Complete using {} measurements.".format(n_meas))
 
         if n_meas < 3:
             print("Logger: System is unobservable. Number of measurements is too low.")
@@ -130,8 +56,6 @@ def main(config):
         )
         socket.send_json(json_msg)
         print("Logger: Data published")
-        n_wait += 1
-        time.sleep(1)
 
 
 if __name__ == "__main__":
